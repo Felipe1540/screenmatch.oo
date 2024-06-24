e07a78eaeb36358854d56a1cadacfbc0cca3d877
Felipe_1540 <felipe_1540@outlook.com>
2024-06-24 20:32:24 -0300

    starting development

---
+++ src/Film.java
@@ -0,0 +1,8 @@
+public class Film {
+    String name;
+    int anoDeLancamento; //releaseYear;
+    boolean inclusoNoPlano; //included;
+    boolean avaliacao; //rate;
+    int totalDeAvaliacoes; //totalRate
+    int duracaoEmMinutos; //durationInMin;
+}
