bfea963ee332c940fb7c181d78c470a60fab9f0f
Felipe_1540 <felipe_1540@outlook.com>
2024-06-24 21:20:04 -0300

    adicionando mais conceitos básicos do estudo

--- src/Film.java
+++ src/Film.java
@@ -2,7 +2,21 @@ public class Film {
     String name;
     int anoDeLancamento; //releaseYear;
     boolean inclusoNoPlano; //included;
-    boolean avaliacao; //rate;
+    double somaDasAvaliacoes; //rate;
     int totalDeAvaliacoes; //totalRate
     int duracaoEmMinutos; //durationInMin;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + name);
+        System.out.println("Ano de lançamento: " + inclusoNoPlano);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes ++;
+    }
+
+    double retornaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         Film meuFilme = new Film();
+
+        meuFilme.name = "A volta dos que não foram";
+        meuFilme.anoDeLancamento = 2000;
+        meuFilme.duracaoEmMinutos = 230;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(3);
+        meuFilme.avalia(5);
+        meuFilme.avalia(7);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.retornaMedia());
+
     }
 }
