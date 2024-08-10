6c4292402ca0d138903470e2a9bb5757038f8341
Felipe_1540 <felipe_1540@outlook.com>
2024-08-10 19:40:07 -0300

    colocando atributos como privados

--- src/Film.java
+++ src/Film.java
@@ -1,30 +1,73 @@
 public class Film {
-    String name;
-    int anoDeLancamento; //releaseYear;
-    boolean inclusoNoPlano; //included;
+    private String name;
+    private int anoDeLancamento; //releaseYear;
+    private boolean inclusoNoPlano; //included;
     private double somaDasAvaliacoes; //rate;
     private int totalDeAvaliacoes; //totalRate
-    int duracaoEmMinutos; //durationInMin;
+    private int duracaoEmMinutos; //durationInMin;
+
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + name);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+        System.out.println("Incluído no plano: " + inclusoNoPlano);
+    }
 
     int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
     }
 
-    double getSomaDasAvaliacoes() {
+   public double getSomaDasAvaliacoes() {
         return somaDasAvaliacoes;
     }
 
-    void exibeFichaTecnica() {
-        System.out.println("Nome do filme: " + name);
-        System.out.println("Ano de lançamento: " + inclusoNoPlano);
-    }
-
-    void avalia(double nota) {
+   public void avalia(double nota) {
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes ++;
     }
 
     double retornaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isInclusoNoPlano() {
+        return inclusoNoPlano;
+    }
+
+    public void setInclusoNoPlano(boolean inclusoNoPlano) {
+        this.inclusoNoPlano = inclusoNoPlano;
+    }
+
+    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+        this.somaDasAvaliacoes = somaDasAvaliacoes;
+    }
+
+    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+        this.totalDeAvaliacoes = totalDeAvaliacoes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Film meuFilme = new Film();
 
-        meuFilme.name = "A volta dos que não foram";
-        meuFilme.anoDeLancamento = 2000;
-        meuFilme.duracaoEmMinutos = 230;
+        meuFilme.setName("A volta dos que não foram");
+        meuFilme.setAnoDeLancamento(2000);
+        meuFilme.setDuracaoEmMinutos(230);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -16,5 +16,18 @@ public class Main {
         System.out.println(meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.retornaMedia());
 
+        Film favorito = new Film();
+
+        favorito.setName("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setInclusoNoPlano(true);
+
+        favorito.exibeFichaTecnica();
+        favorito.avalia(9);
+        favorito.avalia(8);
+        favorito.avalia(9);
+
+        System.out.println("Média de avaliações do filme: " +favorito.retornaMedia());
     }
 }
